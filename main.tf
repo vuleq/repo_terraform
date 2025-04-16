e147a0c759edf54043db1fef96930a47442f023d
Vule <lqvu@tma.com.vn>
2025-04-16 18:46:33 +0700

    update again2

--- main.tf
+++ main.tf
@@ -23,10 +23,10 @@ resource "azurerm_subnet" "vule_subnet" {
 }
 
 resource "azurerm_network_security_group" "vule_nsg" {
-  name                = var.nsg_name
+  name                = "vuleNSG"
   location            = azurerm_resource_group.vule_rg.location
   resource_group_name = azurerm_resource_group.vule_rg.name
-}
+
   security_rule {
     name                       = "AllowSSH"
     priority                   = 1001
@@ -37,4 +37,17 @@ resource "azurerm_network_security_group" "vule_nsg" {
     destination_port_range     = "22"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowRDP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
