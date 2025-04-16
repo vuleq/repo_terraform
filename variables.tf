d587c6f579f8aa7a5c5b0de58f88825417ee536a
vuleq <153701870+vuleq@users.noreply.github.com>
2025-04-16 17:46:19 +0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,24 +1,59 @@
-variable "location" {
-  description = "Azure region"
-  default     = "East US"
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
 }
 
 variable "resource_group_name" {
-  default = "vuleRG"
+  default     = "vuleRG"
+  description = "Resource Group name"
+}
+
+variable "location" {
+  default     = "East US"
+  description = "Location for Azure resources"
 }
 
 variable "vnet_name" {
-  default = "vuleNetwork"
+  default     = "vuleNetwork"
+}
+
+variable "vnet_address_space" {
+  default     = "10.0.0.0/16"
 }
 
 variable "subnet_name" {
-  default = "vuleSubnet"
-}
-
-variable "address_space" {
-  default = ["10.0.0.0/16"]
+  default     = "vuleSubnet"
 }
 
 variable "subnet_prefix" {
-  default = "10.0.1.0/24"
+  default     = "10.0.1.0/24"
+}
+
+variable "nsg_name" {
+  default     = "vuleNSG"
+}
+
+variable "public_ip_name" {
+  default     = "vuleVMLinuxIP"
+}
+
+variable "nic_name" {
+  default     = "vuleVMLinuxNIC"
+}
+
+variable "vm_name" {
+  default     = "vule-vm-linux"
+}
+
+variable "vm_size" {
+  default     = "Standard_B1ms"
+}
+
+variable "admin_username" {
+  default     = "azureuser"
+}
+
+variable "ssh_key_path" {
+  description = "Path to your local public SSH key"
+  type        = string
 }
