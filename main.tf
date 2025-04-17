b679e9a1498e012ec1c7ed096558a74fe180e767
Vule <lqvu@tma.com.vn>
2025-04-17 10:04:05 +0700

    update main tf file

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ resource "azurerm_subnet" "vule_subnet" {
 }
 
 resource "azurerm_network_security_group" "vule_nsg" {
-  name                = "vuleNSG"
+  name                = var.nsg_name
   location            = azurerm_resource_group.vule_rg.location
   resource_group_name = azurerm_resource_group.vule_rg.name
 
@@ -49,5 +49,66 @@ resource "azurerm_network_security_group" "vule_nsg" {
     destination_port_range     = "3389"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_public_ip" "vule_vm_linux_ip" {
+  name                = var.public_ip_name
+  location            = azurerm_resource_group.vule_rg.location
+  resource_group_name = azurerm_resource_group.vule_rg.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "vule_vm_linux_nic" {
+  name                = var.nic_name
+  location            = azurerm_resource_group.vule_rg.location
+  resource_group_name = azurerm_resource_group.vule_rg.name
+
+  ip_configuration {
+    name                          = "vuleLinuxVMIPConfig"
+    subnet_id                     = azurerm_subnet.vule_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.vule_vm_linux_ip.id
+  }
+}
+
+resource "azurerm_virtual_machine" "vule_vm_linux" {
+  name                  = var.vm_name
+  location              = azurerm_resource_group.vule_rg.location
+  resource_group_name   = azurerm_resource_group.vule_rg.name
+  network_interface_ids = [azurerm_network_interface.vule_vm_linux_nic.id]
+  vm_size               = var.vm_size
+
+  os_profile {
+    computer_name  = var.vm_name
+    admin_username = var.admin_username
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = true
+
+    ssh_keys {
+      path     = "/home/${var.admin_username}/.ssh/authorized_keys"
+      key_data = file(var.ssh_key_path)
+    }
+  }
+
+  storage_os_disk {
+    name              = "${var.vm_name}-os-disk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-focal"
+    sku       = "20_04-lts"
+    version   = "latest"
+  }
+
+  tags = {
+    environment = "dev"
+    owner = "vule"
   }
 }
